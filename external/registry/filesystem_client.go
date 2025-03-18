f13a1e81b6d88182ebab6c6cfc5c4d56b3955ff5
Julien-Fruteau <julien.fruteau@gmail.com>
2025-03-19 08:23:13 +1100

    feat: fs cli is gzip file
    test: fs cli is gzip file

--- external/registry/filesystem_client.go
+++ external/registry/filesystem_client.go
@@ -2,8 +2,26 @@ package registry
 
 import (
 	"bytes"
+	"fmt"
 	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/julien-fruteau/go-distribution-registry/internal/env"
 )
+
+var fsBlobsPath = env.GetEnvOrDefault("REG_BLOBS_PATH", "/var/lib/registry/docker/registry/v2/blobs/sha256")
+
+func IsFileGzip(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+	mb := ReadMagicBytes(f)
+	return IsGzipMagicBytes(mb), nil
+}
 
 func ReadMagicBytes(r io.Reader) []byte {
 	buf := make([]byte, 2)
@@ -15,6 +33,30 @@ func IsGzipMagicBytes(b []byte) bool {
 	return bytes.Equal(b, []byte{0x1F, 0x8B})
 }
 
-// TODO: 
-// walk filesystem
-// return blobs digest if gzip
+func WalkDirFn(path string, d fs.DirEntry, err error) error {
+  if err != nil {
+    return err
+  }
+  if d.IsDir() || len(path) <= len(fsBlobsPath)+4 {
+    return nil
+  }
+
+  gzip, err := IsFileGzip(path)
+  if err != nil {
+    return err
+  }
+  if gzip {
+    // TODO: something useful
+    fmt.Println(path)
+  }
+
+	return nil
+}
+
+func WalkFs() error {
+	err := filepath.WalkDir(fsBlobsPath, WalkDirFn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- external/registry/filesystem_client_test.go
+++ external/registry/filesystem_client_test.go
@@ -2,6 +2,9 @@ package registry
 
 import (
 	"bytes"
+	"compress/gzip"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,5 +35,53 @@ func TestMagicBytes(t *testing.T) {
 func TestIsGzipMagicBytes(t *testing.T) {
 	want := []byte{0x1F, 0x8B}
 	assert.True(t, IsGzipMagicBytes(want))
+}
+
+func HelperCreateGzipFile(t testing.TB, path string) {
+	t.Helper()
+	f, err := os.Create(path)
+	assert.NoError(t, err)
+	defer f.Close()
+
+	gzipWriter := gzip.NewWriter(f)
+	defer gzipWriter.Close()
+
+	_, err = gzipWriter.Write([]byte("Hello World"))
+	assert.NoError(t, err)
+}
+
+func HelperCreateTxtFile(t testing.TB, path string) {
+	t.Helper()
+	f, err := os.Create(path)
+	assert.NoError(t, err)
+	defer f.Close()
+
+	_, err = f.Write([]byte("Hello World"))
+	assert.NoError(t, err)
+}
+
+func TestIsGzipFileNotFound(t *testing.T) {
+	_, err := IsFileGzip("not-a-path")
+	assert.ErrorIs(t, err, os.ErrNotExist)
+}
+
+func TestIsGzipFileNo(t *testing.T) {
+	tmpDir := t.TempDir()
+	path := filepath.Join(tmpDir, "test")
+	HelperCreateTxtFile(t, path)
+
+	got, err := IsFileGzip(path)
+	assert.NoError(t, err)
+	assert.False(t, got)
+}
+
+func TestIsGzipFileYes(t *testing.T) {
+	tmpDir := t.TempDir()
+	path := filepath.Join(tmpDir, "test.gz")
+	HelperCreateGzipFile(t, path)
+
+	got, err := IsFileGzip(path)
+	assert.NoError(t, err)
+	assert.True(t, got)
 }
 
